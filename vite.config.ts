bd497d83a6cf107391a0fb9b97fff71cc6835824
Neel <92373023+NboTop@users.noreply.github.com>
2026-06-14 15:55:16 +0530

    Update base path in Vite configuration
    Changed the base path from '/Portfolio/' to '/' in vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Using relative base path allows the app to work at any depth
-  base: '/Portfolio/',
-});
+  base: '/',
+})
